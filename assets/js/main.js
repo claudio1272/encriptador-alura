979bf1331781787aedef609dab6b54a67122b74b
Claudio Díaz <129852561+claudio1272@users.noreply.github.com>
2023-05-30 09:12:39 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,11 @@
 function encrypt() {
   var text = document.getElementById("text-input").value.toLowerCase();
-  //i es para que afecte tanto mayusculas como minusculas
-  //g es para que toda la oración o frase
-  //m es para que todo afecte a multiples lineas o parrafos
-  var encryptedText = text.replace(/e/igm, "enter");
-  var encryptedText = encryptedText.replace(/o/igm, "ober");
-  var encryptedText = encryptedText.replace(/i/igm, "imes");
-  var encryptedText = encryptedText.replace(/a/igm, "ai");
-  var encryptedText = encryptedText.replace(/u/igm, "ufat");
+
+  var encryptedText = text.replace(/e/igm, "enter")
+                          .replace(/o/igm, "ober")
+                          .replace(/i/igm, "imes")
+                          .replace(/a/igm, "ai")
+                          .replace(/u/igm, "ufat");
 
   document.getElementById("right-image").style.display = "none";
   document.getElementById("text-error").style.display = "none";
@@ -17,17 +15,16 @@ function encrypt() {
 
   const txt = document.getElementById("text-input");
   txt.value = "";
-
 }
 
 function decrypt() {
   var text = document.getElementById("text-input").value.toLowerCase();
 
-  var encryptedText = text.replace(/enter/igm, "e");
-  var encryptedText = encryptedText.replace(/ober/igm, "o");
-  var encryptedText = encryptedText.replace(/imes/igm, "i");
-  var encryptedText = encryptedText.replace(/ai/igm, "a");
-  var encryptedText = encryptedText.replace(/ufat/igm, "u");
+  var encryptedText = text.replace(/enter/igm, "e")
+                          .replace(/ober/igm, "o")
+                          .replace(/imes/igm, "i")
+                          .replace(/ai/igm, "a")
+                          .replace(/ufat/igm, "u");
 
   document.getElementById("right-image").style.display = "none";
   document.getElementById("text-error").style.display = "none";
@@ -37,18 +34,11 @@ function decrypt() {
 
   const txt = document.getElementById("text-input");
   txt.value = "";
-
 }
 
-function copy(){
-  var content = document.querySelector("#text-encrypt");
+function copy() {
+  var content = document.getElementById("text-encrypt");
   content.select();
   document.execCommand("copy");
-  Swal.fire(
-    'Copiado!',
-    'El contenido se encuentra en su papelera',
-    'success'
-  )
+  
 }
-
-
